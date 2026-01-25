ff84a587ea102279d69d1f0e4cc0d26c7d4527f6
meko568 <mohammedelbardan82@gmail.com>
2026-01-25 20:54:34 +0200

    updated

--- resources/views/admin/students/edit.blade.php
+++ resources/views/admin/students/edit.blade.php
@@ -46,9 +46,6 @@
                         <div>
                             <label class="block text-sm font-medium text-gray-700">Parent Phone</label>
                             <input type="tel" name="parent_phone" value="{{ old('parent_phone', $student->parent_phone) }}"
-                                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                placeholder="+20 10 1234 5678">
-                            @error('parent_phone')
                                 <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
                             @enderror
                         </div>
