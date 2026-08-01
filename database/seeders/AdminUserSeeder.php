57f8bd440edc2464db0a13c0c99ebf37ca69b4aa
meko568 <mohammedelbardan82@gmail.com>
2026-08-01 06:17:03 +0300

    syntax error fix

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -6,12 +6,15 @@ use App\Models\Student;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
+class AdminUserSeeder extends Seeder
+{
+
     public function run(): void
     {
         Student::updateOrCreate(
             ['role' => 'admin'],
             [
-                'name' => 'Admin'
+                'name' => env('ADMIN_NAME', 'Admin'),
                 'password' => Hash::make(env('ADMIN_PASSWORD', 'Admin@123')),
                 'academicYear' => 'none'
             ]
